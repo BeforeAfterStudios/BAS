798f95d3619d7fa850bf4e40de670afe4983f151
yashdube0911 <ydarts4u@gmail.com>
2023-02-22 21:24:20 +1100

    Header

--- dummy.php
+++
@@ -1,7 +0,0 @@
-<?php
-  header("Cache-Control: no-cache, must-revalidate"); // HTTP 1.1
-  header("Pragma: no-cache"); // HTTP 1.0
-  header("Expires: Wed, 1 Jan 2020 00:00:00 GMT");
-?>
-var foo = "bar";
-console.log(foo);
